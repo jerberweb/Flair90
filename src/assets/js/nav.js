f0c580fbebc9c9829a5fb151c0862b9c9f2b8c2a
Jerber Web Design and Development <110627649+jerberweb@users.noreply.github.com>
2023-12-25 02:32:29 -0500

    Initial Commit

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,33 +1,51 @@
 // add classes for mobile navigation toggling
-var CSbody = document.querySelector('body');
-const CSnavbarMenu = document.querySelector('#cs-navigation');
-const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+var CSbody = document.querySelector("body");
+const CSnavbarMenu = document.querySelector("#cs-navigation");
+const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
-CShamburgerMenu.addEventListener('click', function () {
-	CShamburgerMenu.classList.toggle('cs-active');
-	CSnavbarMenu.classList.toggle('cs-active');
-	CSbody.classList.toggle('cs-open');
-	// run the function to check the aria-expanded value
-	ariaExpanded();
+CShamburgerMenu.addEventListener("click", function () {
+CShamburgerMenu.classList.toggle("cs-active");
+CSnavbarMenu.classList.toggle("cs-active");
+CSbody.classList.toggle("cs-open");
+// run the function to check the aria-expanded value
+ariaExpanded();
 });
 
 // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
 function ariaExpanded() {
-	const csUL = document.querySelector('#cs-expanded');
-	const csExpanded = csUL.getAttribute('aria-expanded');
+const csUL = document.querySelector("#cs-expanded");
+const csExpanded = csUL.getAttribute("aria-expanded");
 
-	if (csExpanded === 'false') {
-		csUL.setAttribute('aria-expanded', 'true');
-	} else {
-		csUL.setAttribute('aria-expanded', 'false');
-	}
+if (csExpanded === "false") {
+	csUL.setAttribute("aria-expanded", "true");
+} else {
+	csUL.setAttribute("aria-expanded", "false");
+}
 }
 
 // mobile nav toggle code
-const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+const dropDowns = Array.from(
+document.querySelectorAll("#cs-navigation .cs-dropdown")
+);
 for (const item of dropDowns) {
-	const onClick = () => {
-		item.classList.toggle('cs-active');
-	};
-	item.addEventListener('click', onClick);
+const onClick = () => {
+	item.classList.toggle("cs-active");
+};
+item.addEventListener("click", onClick);
 }
+
+// Navbar changes on scroll when the div that is created reaches a certain area
+const navbar = document.querySelector("#cs-navigation");
+const scrollwatcher = document.createElement("div");
+
+scrollwatcher.setAttribute("data-scroll-watcher", "");
+navbar.before(scrollwatcher);
+
+const navobserver = new IntersectionObserver(
+(entries) => {
+	navbar.classList.toggle("scrolled", !entries[0].isIntersecting);
+},
+{ rootMargin: "15px 0px 0px 0px" }
+);
+
+navobserver.observe(scrollwatcher);
