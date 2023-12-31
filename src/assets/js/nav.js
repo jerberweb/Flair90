442bd36e83c23c2d5bee811752edc6574f14ab42
Jerber Web Design and Development <110627649+jerberweb@users.noreply.github.com>
2023-12-31 02:15:57 -0500

    news updates

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -4,34 +4,32 @@ const CSnavbarMenu = document.querySelector("#cs-navigation");
 const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
 CShamburgerMenu.addEventListener("click", function () {
-CShamburgerMenu.classList.toggle("cs-active");
-CSnavbarMenu.classList.toggle("cs-active");
-CSbody.classList.toggle("cs-open");
-// run the function to check the aria-expanded value
-ariaExpanded();
+    CShamburgerMenu.classList.toggle("cs-active");
+    CSnavbarMenu.classList.toggle("cs-active");
+    CSbody.classList.toggle("cs-open");
+    // run the function to check the aria-expanded value
+    ariaExpanded();
 });
 
 // checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
 function ariaExpanded() {
-const csUL = document.querySelector("#cs-expanded");
-const csExpanded = csUL.getAttribute("aria-expanded");
+    const csUL = document.querySelector("#cs-expanded");
+    const csExpanded = csUL.getAttribute("aria-expanded");
 
-if (csExpanded === "false") {
-	csUL.setAttribute("aria-expanded", "true");
-} else {
-	csUL.setAttribute("aria-expanded", "false");
-}
+    if (csExpanded === "false") {
+        csUL.setAttribute("aria-expanded", "true");
+    } else {
+        csUL.setAttribute("aria-expanded", "false");
+    }
 }
 
 // mobile nav toggle code
-const dropDowns = Array.from(
-document.querySelectorAll("#cs-navigation .cs-dropdown")
-);
+const dropDowns = Array.from(document.querySelectorAll("#cs-navigation .cs-dropdown"));
 for (const item of dropDowns) {
-const onClick = () => {
-	item.classList.toggle("cs-active");
-};
-item.addEventListener("click", onClick);
+    const onClick = () => {
+        item.classList.toggle("cs-active");
+    };
+    item.addEventListener("click", onClick);
 }
 
 // Navbar changes on scroll when the div that is created reaches a certain area
@@ -42,10 +40,39 @@ scrollwatcher.setAttribute("data-scroll-watcher", "");
 navbar.before(scrollwatcher);
 
 const navobserver = new IntersectionObserver(
-(entries) => {
-	navbar.classList.toggle("scrolled", !entries[0].isIntersecting);
-},
-{ rootMargin: "15px 0px 0px 0px" }
+    (entries) => {
+        navbar.classList.toggle("scrolled", !entries[0].isIntersecting);
+    },
+    { rootMargin: "15px 0px 0px 0px" }
 );
 
 navobserver.observe(scrollwatcher);
+
+// <!-- ============================================ -->
+// <!--             Background Change                -->
+// <!-- ============================================ -->
+
+// Changes blog pages background on recent articles pages and the individual article page in regards to the url
+document.addEventListener("DOMContentLoaded", function () {
+    // Check the current URL
+    checkURL();
+
+    // Listen for changes in the URL (e.g., when navigating between pages)
+    window.addEventListener("popstate", checkURL);
+});
+
+function checkURL() {
+    // Get the current URL
+    var currentURL = window.location.pathname;
+
+    // Check if the URL starts with "/news/"
+    if (currentURL.startsWith("/news/")) {
+        // Apply the appropriate background color class to #main
+        document.getElementById("main").classList.remove("black-bg");
+        document.getElementById("main").classList.add("yellow-bg");
+    } else {
+        // Default background color for other pages
+        document.getElementById("main").classList.remove("yellow-bg");
+        document.getElementById("main").classList.add("black-bg");
+    }
+}
